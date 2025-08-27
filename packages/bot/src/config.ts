429d894f25eef27a0bbe4600a898f08fa4d6b858
Ducko <duckodas@gmail.com>
2025-08-27 16:22:36 +0200

    Update config.ts

--- packages/bot/src/config.ts
+++ packages/bot/src/config.ts
@@ -9,7 +9,9 @@ export type webhookArray = Array<{ name: string; id: string; token: string }>;
 export interface ConfigInterface {
 	client: { token: string; id: string; secret: string };
 	guilds: ObjectNameIDArray;
+	channels: ObjectNameIDArray;
 	webhooks: webhookArray;
+	APIs: ObjectNameIDArray;
 }
 
 export const config: ConfigInterface = {
@@ -18,6 +20,29 @@ export const config: ConfigInterface = {
 		id: process.env.CLIENT_ID as string,
 		secret: process.env.CLIENT_SECRET as string,
 	},
-	guilds: [],
+	guilds: [
+		{
+			name: 'Milkshake Collective',
+			id: '1396235829579485214',
+		},
+		{
+			name: 'Online Safety',
+			id: '1360001636424093928',
+		},
+	],
+	channels: [
+		{
+			name: 'Weekly Report Channel',
+			id: process.env.WEEKLY_REPORT_CHANNEL_ID,
+		},
+		{
+			name: 'Appeal Log Channel',
+			id: process.env.APPEAL_LOG_CHANNEL_ID,
+		},
+	],
 	webhooks: [],
+	APIs: [
+		{ name: 'Perspective API Key', id: process.env.PERSPECTIVE_API_KEY },
+		{ name: 'OpenAI API Key', id: process.env.OPENAI_API_KEY },
+	],
 };
