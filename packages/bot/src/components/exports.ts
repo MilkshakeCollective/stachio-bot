26cc9722ce0dac1506e5c0aba980113b49d1ed8d
Ducko <duckodas@gmail.com>
2025-09-01 22:22:03 +0200

    Update exports.ts

--- packages/bot/src/components/exports.ts
+++ packages/bot/src/components/exports.ts
@@ -1,4 +1,4 @@
-export { installGuild, uninstallGuild, hasRoles } from './helpers/discord.js';
+export { addWarning, enforceSanctions, installGuild, uninstallGuild, hasRoles } from './helpers/discord.js';
 export { logger } from './helpers/logger.js';
 export { toStringArray } from './helpers/misc.js';
 export { sendWatchdogReport, actionUser } from './helpers/watchdog.js';
